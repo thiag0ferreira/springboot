df2814604adb87b07e8d19bdbc86410a054ed02b
Thiago <thiag0ferreira@hotmail.com>
2023-11-07 16:19:47 -0300

    pequeno ajuste2

--- carros/src/main/java/com/example/carros/domain/CarroService.java
+++ carros/src/main/java/com/example/carros/domain/CarroService.java
@@ -2,9 +2,6 @@ package com.example.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
