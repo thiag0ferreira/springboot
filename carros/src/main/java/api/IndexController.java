9006659676c0cf7078d9dd2245853a60dc84ae59
Thiago <thiag0ferreira@hotmail.com>
2023-11-07 00:46:04 -0300

    first commit

---
+++ carros/src/main/java/api/IndexController.java
@@ -0,0 +1,16 @@
+package api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class IndexController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+}
